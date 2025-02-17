3aa5c96c6ce3c73b9eeb8e8b63285d1ec1404da9
Mhhxl <166766597+Mhhxl@users.noreply.github.com>
2025-02-17 19:48:22 +0000

    salvando

---
+++ 2.estrutura condicional/1.estrutura_simples.py
@@ -0,0 +1,6 @@
+import os
+os.system ("clear")
+
+idade = 8
+if idade < 10:
+    print("ACESSO NEGADO!")
---
+++ 2.estrutura condicional/2.estrutura_composta.py
@@ -0,0 +1,10 @@
+import os
+os.system ("clear")
+
+idade = 11
+if idade < 10:
+    print("ACESSO NEGADO!")
+else:print("ACESSO PERMITIDO!")
+
+print("==FIM==")
+
