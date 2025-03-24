a32b3214995c6a540c7b8e2d8628e20fb9716faa
Mhhxl <166766597+Mhhxl@users.noreply.github.com>
2025-03-24 19:58:53 +0000

    salvando

---
+++ 5.laço de repetição/while/atividades/atv6.py
@@ -0,0 +1,13 @@
+import os
+os.system('cls || clear')
+
+soma = 0
+nota_adicional= 0
+
+#entrada
+nota= float(input("Digite a nota: "))
+while True:
+    if nota_adicional == "n | N":
+        break
+    
+      
