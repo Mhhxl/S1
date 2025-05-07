e16f4d1e00d00d25ee4f6ffc901f6b5606fe2c4a
Mhhxl <166766597+Mhhxl@users.noreply.github.com>
2025-05-07 19:33:35 +0000

    salvando

--- 7. vetores/atv5.py
+++ 7. vetores/atv5.py
@@ -1,23 +1,8 @@
 import os
 os.system('cls || clear')
 
-def par_ou_impar(num):
-   
-    par = 0
-    impar = 0
-    if num % 2 == 0:
-        par += 1
-    else:
-        impar += 1
-        return par, impar
-#criando lista de numeros
-lista_num= []
-quantidade_numeros = 6
-par_ou_impar = (lista_num)
-print("== SOLICITANDO NUMEROS==")
-for i in range(quantidade_numeros):
-    num = int(input(f"Digite o {i+1}º número: "))
-    lista_num.append(num)
 
-print(f"pares :{par_ou_impar(lista_num)}")
-print(f"impares :{par_ou_impar(lista_num)}")
+QUANTIDADE_NUMEROS = 6
+lista_numeros = []
+pares = 0
+impares = 0
